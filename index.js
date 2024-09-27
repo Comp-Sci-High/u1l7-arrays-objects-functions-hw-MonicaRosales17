30939c9c22d8f8cffe0abe00e3b5279fc8ea0791
MonicaRosales17 <monica.rosaleshernandez25@compscihigh.org>
2024-09-27 01:22:36 +0000

    FInished homework

--- index.js
+++ index.js
@@ -9,7 +9,17 @@
 // E. Delete the last fruit in the array.
 // F. Log the updated array.
 
+let fruits = ["apple", "banana", "cherry"]
+console.log(fruits[1])
 
+fruits[2] = "orange"
+// console.log(fruits)
+
+fruits.push("grape")
+// console.log(fruits)
+
+fruits.pop()
+console.log(fruits)
 
 // TASK 2: Objects - Create, Read, Update, Delete (CRUD)
 // A. Create an object called `student` with the keys: name, age, and grade. (add your own values)
@@ -19,12 +29,27 @@
 // E. Delete the `age` property from the object.
 // F. Log the updated student object.
 
+let student = {
+   name: "Monica",
+   age: 17,
+   grade: "A"
+}
 
+console.log(student.age)
+
+student.grade = "A+"
+student.isGraduated = true
+delete student.age
+
+console.log(student)
 
 // TASK 3: Functions - Create and Call Functions
 // A. Write a function called `calculateTotal` that takes two inputs: price and quantity.
 //    The function should return the total price by multiplying price by quantity.
 
+function calculateTotal(price, quantity){
+   return price * quantity
+}
 
 
 // B. Call the `calculateTotal` function 3 times and console log the outputs
@@ -32,6 +57,9 @@
 // Second with the inputs 12 and 8
 // Third with the inputs 9 and 42
 
+console.log(calculateTotal(20, 5))
+console.log(calculateTotal(12, 8))
+console.log(calculateTotal(9, 42))
 
 /*
    ==== Final Commit Instructions ====
